7cfbd259a93a37914883cf133ef46dd0f1e42111
astor <astor@proliant.(none)>
2014-02-19 03:18:41 +0100

    some code cleaning and beginning of themes handling

--- hue-remote.py
+++ hue-remote.py
@@ -36,58 +36,59 @@ class hueparam:
     	self.buff = self.mini
     return self
 
-
   def __repr__(self):
     """Return attributes as dict without methods"""
     return dict((key, getattr(self, key)) for key in dir(self) if key not in dir(self.__class__))
+
   def __str__(self):
     """return name and """
-    return " ".join([self.name, ":", str(self.buff), "\t| last update :", str(self.last_update)]) 
+    return " ".join(["[hueparam]", self.name, ":", str(self.buff), "\t| last update :", str(self.last_update)]) 
+
+
 
 def switch_lights(hue):
+  """Switch all lights on if they are off, and vice versa"""
   is_on = hue.get_group(LIGHT_GROUP, "on")
   hue.set_group(LIGHT_GROUP, "on", not(is_on))
 
 def change_param(hue, param, val):
+  """Change a specific parameter of the lamps
+    I'm delaying the update frequency as the device sends input much more
+    rapidly than the Philips Hue api is able to handle
+
+    I'm using set_light instead of set_group for the same reason, e.g :
+    "We can’t send commands to the lights too fast. If you stick to around
+     10 commands per second to the /lights resource as maximum you should be fine.
+     For /groups commands you should keep to a maximum of 1 per second."
+    http://developers.meethue.com/coreconcepts.html
+  """
   param += val
   if (datetime.now() - param.last_update).microseconds > 100000:
-    curr_val = hue.get_group(LIGHT_GROUP, param.name)
+    curr_val = hue.get_light(2, param.name)
     if curr_val != param.buff:
       logger.debug(param)
       for i in range(1,4):
 	hue.set_light(i, param.name, param.buff)
       param.last_update = datetime.now()
-      param.changed = False
 
 
-def change_cie(hue, val, code):
-  global x_offset, y_offset
-  if code == ecodes.REL_X:
-    x_offset += val / 1000.0
-  else:
-    y_offset += val / 1000.0
-  
-  if (x_offset >= 0.02) or (x_offset <= -0.02) or (y_offset >= 0.02) or (y_offset <= -0.02):
-    curr_x, curr_y = hue.get_group(LIGHT_GROUP, "xy")
-    new_x = curr_x + x_offset
+def change_theme(hue, themes, index):
+  theme_name = themes.keys()[theme_index]
+  th = themes[theme_name]
+  size = len(th)
+  logger.debug("***Changing theme to " + theme_name + "***")
 
-    if new_x > 1.0:
-      new_x = 1.0
-    if new_x < 0.0:
-      new_x = 0.0      
-    
-    new_y = curr_y + y_offset
-    if new_y > 1.0:
-      new_y = 1.0
-    if new_y < 0.0:
-      new_y = 0.0
+  for i in [1,2,3]:
+    lamp_th = th[i % size]
+    if lamp_th.has_key("xy"):
+      hue.set_light(i, "xy", [lamp_th["xy"][0], lamp_th["xy"][1]])
+    elif lamp_th.has_key("ct"):
+      hue.set_light(i, "ct", lamp_th["ct"])
+    elif lamp_th.has_key("hue"):
+      hue.set_light(i, "hue", lamp_th["hue"])
+      if lamp_th.has_key("sat"):
+        hue.set_light(i, "sat", lamp_th["sat"])
 
-    #hue.set_group(LIGHT_GROUP, "xy", [new_x, new_y])
-    logger.debug("Change  x,y (" + str(curr_x) + "," + str(curr_y) + ") to (" + str(new_x) + ";" + str(new_y) + ")")
-    for i in [1,2,3]:
-      hue.set_light(i, "xy", [new_x, new_y])
-    x_offset = 0.0
-    y_offset = 0.0
 
 
 if __name__ == "__main__":
@@ -102,6 +103,18 @@ if __name__ == "__main__":
   logfile.setFormatter(logformat)
   logger.addHandler(logfile)
 
+  themes = dict()
+  themes["relax"] = [{"ct": 2431}]
+  themes["energize"] = [{"ct": 6474}]
+  themes["deep_sea"] = [{"xy": (0.1859, 0.0771)}, {"xy": (0.6367, 0.3349)}, {"xy": (0.1859, 0.0771)}]
+  themes["blue_rain"] = [{"xy": (0.1859, 0.0771)}]
+  themes["laila"] = [{"xy": (0.4071, 0.514)}, {"ct": 3861}, {"xy": (0.4071, 0.514)}]
+  themes["sunset"] = [{"ct": 2535}, {"xy": (0.6349, 0.3413)}, {"ct": 2535}]
+  themes["love_shack"] = [{"hue": 53498, "sat": 254}, {"hue": 48401, "sat": 254}, {"hue": 53498, "sat": 254}] 
+  themes["ultraviolet"] = [{"hue": 45831, "sat": 254}, {"hue": 48401, "sat": 254}, {"hue": 50671, "sat": 254}] 
+  themes["candles"] = [{"hue": 4326, "sat": 254}, {"hue": 2141, "sat": 254}, {"hue": 4326, "sat": 254}]
+  theme_index = 0
+
   params = dict()
   params["brightness"] = hueparam("bri", 255, 4)
   params["hue"] = hueparam("hue", 65535, 20, modulo=True)
@@ -112,18 +125,19 @@ if __name__ == "__main__":
 
   for event in remote.read_loop():
     if event.type in [ecodes.EV_KEY, ecodes.EV_REL]:
-      if event.code == ecodes.BTN_RIGHT:
-        if event.value == 1:
-          logger.debug("BTN_RIGHT")
-          switch_lights(hue)
+      if event.code == ecodes.BTN_RIGHT and event.value == 1:
+        logger.debug("Switching lights")
+        switch_lights(hue)
       elif event.code == ecodes.BTN_LEFT:
         print("todo")
-      elif event.code == ecodes.BTN_SIDE: 
+      elif event.code == ecodes.BTN_SIDE and event.value == 1: 
       # 3 doigts de droite à gauche
-        print("todo")
-      elif event.code == ecodes.BTN_EXTRA: 
+        theme_index = (theme_index - 1) % len(themes)
+        change_theme(hue, themes, theme_index)
+      elif event.code == ecodes.BTN_EXTRA and event.value == 1: 
       # 3 doigts de gauche ) droite
-        print("todo")
+        theme_index = (theme_index + 1) % len(themes)
+        change_theme(hue, themes, theme_index)
       elif event.code == ecodes.KEY_PAGEUP: 
       # 3 doigts de bas en haut
         print("todo")
